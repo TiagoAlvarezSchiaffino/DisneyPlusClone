d7b66c81c6bb918b79622f7330eb1b176de7f436
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-02-17 20:58:55 -0300

    feat: Add SearchInput component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
 import { ThemeToggler } from "./ThemeToggler"
+import SearchInput from "./SearchInput"
 
 function Header() {
   return (
-    <header>
-        <Link href="/">
+    <header className="fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
+        <Link href="/" className="mr-10">
             <Image
                 src="/disneylogo.png"
                 alt="Disney Logo"
@@ -17,7 +18,7 @@ function Header() {
 
         <div className="flex space-x-2">
             {/* GenreDropdown */}
-            {/* SearchInput */}
+            <SearchInput />
             <ThemeToggler />
         </div>
     </header>

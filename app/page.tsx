f26a6ce4f3ee667406f1a7c5e441b3fca1268572
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-02-19 19:33:02 -0300

    feat: Integrating MovieCarousel component into page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,14 @@ export default function Home() {
   return (
     <main className="">
       <h1 className=""></h1>
+
+      {/* CarouselBannerWrapper */}
+
+      <div>
+        {/* <MovieCarousel movies={...} title='Upcoming' /> */}
+        {/* <MovieCarousel movies={...} title='Upcoming' /> */}
+        {/* <MovieCarousel movies={...} title='Upcoming' /> */}
+      </div>
     </main>
   );
 }

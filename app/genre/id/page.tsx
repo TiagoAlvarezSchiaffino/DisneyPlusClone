15dbe7b179209829b078788cb6abad06fc3b97ae
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-02-20 19:02:52 -0300

    feat: Implement dynamic movie fetching

--- app/genre/id/page.tsx
+++ app/genre/id/page.tsx
@@ -1,3 +1,6 @@
+import MoviesCarousel from "@/components/MoviesCarousel"
+import { getDiscoverMovies } from "@/lib/getMovies"
+
 type Props = {
     params: {
         id: string
@@ -7,12 +10,18 @@ type Props = {
     }
 }
 
-function genrePage({params: { id }, searchParams: { genre }}: Props) {
+async function GenrePage({params: { id }, searchParams: { genre }}: Props) {
+  const movies = await getDiscoverMovies(id)
+  
   return (
-  <div>
-    Welcome to the genre with ID: { id } and name: { genre }
+  <div className="maw-w-7xl mx-auto">
+    {/* Azure OPENAI service Azure Suggestion */}
+    <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+      <h1 className="text-6-xl font-bold px-10">Results for {genre}</h1>
+      <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+    </div>
   </div>
   )
 }
 
-export default genrePage
+export default GenrePage

4c79822f1a12941152d4004a34ada38ac58495c3
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-02-20 22:54:34 -0300

    feat: Implementing OpenAI API KEY

--- function/src/functions/getAISuggestion.ts
+++ function/src/functions/getAISuggestion.ts
@@ -1,8 +1,10 @@
+// https://azure-disney-clone.azurewebsites.net/api/getaisuggestion
+
 import {
     app,
     HttpRequest,
     HttpResponseInit,
-    InvocationContext
+    InvocationContext,
 } from "@azure/functions";
 
 import OpenAI from "openai"
@@ -17,13 +19,29 @@ export async function getAISuggestion(
 ): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get("name") || await request.text() || "world";
+    const term = request.query.get("term")
 
-    return { body: `Hello, ${name}!` };
+    const completion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: "system",
+                content: `You are a digital video assistant working for services such as Netflix, Disney Plus & Amazon Prime Video. Your job is to provide suggestions based on the videos the user specifies. Provide an quirky breakdown of what the user should watch next! It should only list the names of the films after the introduction. Keep the response short and sweet! Always list at least 3 films as suggestions. If the user mentions a genre, you should provide a suggestion based on that genre.`
+            },
+            {
+                role: "user",
+                content: `I like: ${term}`,
+            }
+        ],
+    })
+
+    console.log(completion.choices[0])
+
+    return { body: completion.choices[0].message.content || "No suggestion" };
 };
 
 app.http("getAISuggestion", {
-    methods: ["GET", "POST"],
+    methods: ["GET"],
     authLevel: "anonymous",
     handler: getAISuggestion
 });

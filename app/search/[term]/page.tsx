b6b1b7a33237dfd7a61d3eb37ec1717fb79783cd
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-02-21 16:01:43 -0300

    feat: Implement useSWR for data fetching

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,3 +1,4 @@
+import AISuggestion from "@/components/AISuggestion"
 import MoviesCarousel from "@/components/MoviesCarousel"
 import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies"
 import { notFound } from "next/navigation"
@@ -21,7 +22,7 @@ async function SearchPage({ params: { term } }: Props) {
             <div className=" flex flex-col space-y-4 mt-32 xl:mt-42">
                 <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
 
-                {/* AI Suggestion */}
+                <AISuggestion term={termToUse} />
 
                 <MoviesCarousel title="Movies" movies={movies} isVertical />
                 <MoviesCarousel title="You may also like" movies={popularMovies} />

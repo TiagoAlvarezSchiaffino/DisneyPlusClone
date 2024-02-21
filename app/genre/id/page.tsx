19009bf4847ed4d4474c2285836d5808445a78db
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-02-21 16:55:46 -0300

    refactor: Integrate OpenAIAzureSuggestion into GenrePage

--- app/genre/id/page.tsx
+++ app/genre/id/page.tsx
@@ -1,26 +1,28 @@
 import MoviesCarousel from "@/components/MoviesCarousel"
+import OpenAIAzureSuggestion from "@/components/OpenAIAzureSuggestion"
 import { getDiscoverMovies } from "@/lib/getMovies"
 
 type Props = {
     params: {
         id: string
-    },
+    }
     searchParams: {
         genre: string
     }
 }
 
-async function GenrePage({params: { id }, searchParams: { genre }}: Props) {
+async function GenrePage({ params: { id }, searchParams: { genre }}: Props) {
   const movies = await getDiscoverMovies(id)
   
   return (
-  <div className="maw-w-7xl mx-auto">
-    {/* Azure OPENAI service Azure Suggestion */}
-    <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
-      <h1 className="text-6-xl font-bold px-10">Results for {genre}</h1>
-      <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+    <div className="maw-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6-xl font-bold px-10">Results for {genre}</h1>
+        <OpenAIAzureSuggestion term={genre} />
+
+        <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+      </div>
     </div>
-  </div>
   )
 }
 
